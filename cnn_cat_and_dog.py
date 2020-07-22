c300384d88be79c68e99426d10f2bf17e3a6886a
ravimalvia <53039826+ravimalvia@users.noreply.github.com>
2020-07-22 14:00:46 +0530

    update

--- cnn_cat_and_dog.py
+++ cnn_cat_and_dog.py
@@ -65,19 +65,19 @@ train_datagen = ImageDataGenerator(
         horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory(
-        'training_set/',
+        '/root/training_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 test_set = test_datagen.flow_from_directory(
-        'test_set/',
+        '/root/test_set/',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 model.fit(
         training_set,
         steps_per_epoch=8000,
-        epochs=4,
+        epochs=1,
         validation_data=test_set,
         validation_steps=800)
 
